5e9560d530fde2c698b1a7aec6d63b2d05f91643
Andrey Volokhin <nedudos@gmail.com>
2023-09-20 13:46:06 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Cho po kaifu
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+    echo "Hello world <br>";
+    // My comment
+    /*  
+        My
+        Big
+        Comment
+    */
+
+    $my_name = "Andrey";
+
+    echo "Hello {$my_name}";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button> Press Me </button>
+</body>
+</html>
